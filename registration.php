40bbda878c2405eba82e835720824d09d194156c
Trần Khánh Đăng <dangtran.mkd@gmail.com>
2022-08-08 15:30:48 +0700

    commit dev

--- registration.php
+++ registration.php
@@ -4,5 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'MKD_MageplazaPreOrder',
+    'MKD_MageplazaPreOrder',
     __DIR__
 );
